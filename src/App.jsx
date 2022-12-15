bc21b7fe9c43817ebbbf81833d5b6334decceff5
kimsangjunv1 <ingyoe64@gmail.com>
2022-12-15 21:50:51 +0900

    2022.12.15

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import { useEffect } from "react";
-// import reactLogo from "./assets/react.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import "./App.css";
+
 import Main from "./components/pages/Main";
 import gsaps from "./components/utils/gsaps";
 import gsapAnimation from "./components/utils/gsapAnimation";
 import lenis from "./components/utils/lenis";
 import section from "./components/utils/section";
-import imgLoader from "./components/utils/imgLoader";
 
 function App() {
-  // const [count, setCount] = useState(0);
   useEffect(() => {
     gsaps();
     gsapAnimation();
     section();
     lenis();
-
-    // imgLoader();
   }, []);
 
   return (
@@ -26,28 +21,6 @@ function App() {
         <Route path="/" element={<Main />} />
       </Routes>
     </BrowserRouter>
-    // <div className="App">
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </div>
   );
 }
 
