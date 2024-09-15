eea4b1beb59081670e3aea55890297e1a3c8bf23
김상준 <ingyoe64@gmail.com>
2024-09-15 22:10:17 +0900

    [Docs] 스킬 섹션 설명 수정

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -47,131 +47,131 @@ export const data = {
     // 섹션 3
     skill: [
         {
-            title: "라이브러리",
-            desc: [
+            "title": "라이브러리",
+            "desc": [
                 {
-                    name:"Bootstrap",
-                    description:"클래스명으로도 간편하게 html에 살을 입힐 수 있어 사용하게 되었습니다.",
-                    imgSrc: "img-icon-react"
+                    "name": "Bootstrap",
+                    "description": "클래스명으로 간편하게 HTML을 스타일링할 수 있어 사용했습니다.",
+                    "imgSrc": "img-icon-react"
                 },
                 {
-                    name:"Laravel Blade Template",
-                    description:"라라벨에서 효율적인 HTML 작성을 위해 조건문, 반복문 등의 기본적인 명령문들을 사용 할 수 있습니다.",
-                    imgSrc: "img-icon-laravel"
+                    "name": "Laravel Blade Template",
+                    "description": "라라벨에서 효율적인 HTML 작성을 위해 조건문과 반복문 등을 사용했습니다.",
+                    "imgSrc": "img-icon-laravel"
                 },
                 {
-                    name:"Lottie",
-                    description:"이미지에 모션을 넣어 gif 대비 적은 용량, 최적화 면에 많은 매력을 느껴 사용하게 되었습니다.",
-                    imgSrc: "img-icon-laravel"
+                    "name": "Lottie",
+                    "description": "모션을 추가하여 GIF보다 적은 용량으로 최적화된 이미지를 제공하는 데 매력을 느껴 사용했습니다.",
+                    "imgSrc": "img-icon-laravel"
                 },
                 {
-                    name: "Swiper",
-                    description: "다양한 데모를 통한 여러가지 형태의 슬라이드 구현이 가능하여 사용하게 되었습니다",
-                    imgSrc: "img-icon-swiper"
+                    "name": "Swiper",
+                    "description": "다양한 슬라이드 형태를 구현할 수 있어 사용했습니다.",
+                    "imgSrc": "img-icon-swiper"
                 },
                 {
-                    name: "Lenis",
-                    description: "스크롤을 부드럽게 제어하기 간편해 사용하게 되었습니다",
-                    imgSrc: "img-icon-lenis"
+                    "name": "Lenis",
+                    "description": "부드러운 스크롤 제어가 간편하여 사용했습니다.",
+                    "imgSrc": "img-icon-lenis"
                 },
                 {
-                    name: "Jquery",
-                    description: "바닐라 자바스크립트를 좀 더 손쉽게 사용 가능하고, 특정한 애니메이션을 구현하는데 소요되는 시간이 적어 사용하였습니다.",
-                    imgSrc: "img-icon-jquery"
-                },
+                    "name": "Jquery",
+                    "description": "바닐라 자바스크립트를 더 쉽게 사용하고, 애니메이션 구현 시간이 단축되어 사용했습니다.",
+                    "imgSrc": "img-icon-jquery"
+                }
             ]
         },
         {
-            title: "프론트",
-            desc: [
+            "title": "프론트",
+            "desc": [
                 {
-                    name:"React",
-                    description:"React의 구조를 이해하고 있으며, 여러가지 Hook을 사용할 수 있습니다.",
-                    imgSrc: "img-icon-react"
+                    "name": "React",
+                    "description": "React의 구조를 이해하고 여러 Hook을 사용하여 개발했습니다.",
+                    "imgSrc": "img-icon-react"
                 },
                 {
-                    name:"ReduxToolKit",
-                    description:"한곳에 집중된 상태관리 덕분에 에러가 발생한 경우 찾기 수월해 사용하였습니다.",
-                    imgSrc: "img-icon-react"
+                    "name": "ReduxToolKit",
+                    "description": "집중된 상태 관리를 통해 에러 추적이 용이하여 사용했습니다.",
+                    "imgSrc": "img-icon-react"
                 },
                 {
-                    name:"Zustand",
-                    description:"리덕스를 사용하기 위해 provider 등등의<br />초기 세팅 해야하는 과정들을 축소시켜 빠른 작업이 가능하였고<br />상태를 정의하고 사용하는 방법이 단순해 사용하게 되었습니다!",
-                    imgSrc: "img-icon-react"
+                    "name": "Zustand",
+                    "description": "리덕스의 초기 세팅을 축소하여 빠르게 작업할 수 있었고, 상태 정의와 사용이 간편해 사용했습니다.",
+                    "imgSrc": "img-icon-react"
                 },
                 {
-                    name: "Svelte",
-                    description: "간단한 가능",
-                    imgSrc: "img-icon-svelte"
+                    "name": "Svelte",
+                    "description": "간단한 상태 관리와 반응형 기능을 제공하여 사용했습니다.",
+                    "imgSrc": "img-icon-svelte"
                 },
                 {
-                    name: "Next JS",
-                    description: "간단한 가능",
-                    imgSrc: "img-icon-svelte"
-                },
+                    "name": "Next JS",
+                    "description": "서버 사이드 렌더링과 정적 사이트 생성 기능을 활용해 사용했습니다.",
+                    "imgSrc": "img-icon-svelte"
+                }
             ]
         },
         {
-            title: "백엔드",
-            desc: [
+            "title": "백엔드",
+            "desc": [
                 {
-                    name: "PHP",
-                    description: "dothome을 통해 간단하게 회원들의 정보를 관리하고 게시물 작성 및 삭제 할 수 있어 사용하게 되었습니다",
-                    imgSrc: "img-icon-php"
+                    "name": "PHP",
+                    "description": "Dothome을 통해 회원 정보 관리와 게시물 작성 및 삭제를 구현했습니다.",
+                    "imgSrc": "img-icon-php"
                 },
                 {
-                    name: "MySQL",
-                    description: "php로 작성한것을 dothome 호스팅을 통해 filezilla로 접속하여 관리 하기위해 사용하였습니다.",
-                    imgSrc: "img-icon-php"
-                },
+                    "name": "MySQL",
+                    "description": "PHP로 작성한 데이터를 Dothome 호스팅을 통해 관리했습니다.",
+                    "imgSrc": "img-icon-php"
+                }
             ]
         },
         {
-            title: "디자인",
-            desc: [
+            "title": "디자인",
+            "desc": [
                 {
-                    name:"Figma",
-                    description:"adobe xd가 아닌 웹사이트를 손쉽게 디자인하고 같은 반복적인 작업이 필요할떄는 컴포넌트별로 분리 후 하나만 수정하면 다양한것들이 수정 가능한 기능들이 제공되어 사용하게 되었습니다.",
-                    imgSrc: "img-icon-figma"
+                    "name": "Figma",
+                    "description": "반복 작업을 효율적으로 처리할 수 있도록 컴포넌트를 분리하여 디자인했습니다.",
+                    "imgSrc": "img-icon-figma"
                 },
                 {
-                    name:"Framer",
-                    description:"Lottie를 위한 모션 작업이 가능합니다.",
-                    imgSrc: "img-icon-photoshop"
+                    "name": "Framer",
+                    "description": "Lottie 모션 작업을 구현하기 위해 사용했습니다.",
+                    "imgSrc": "img-icon-photoshop"
                 },
                 {
-                    name:"Photoshop",
-                    description:"간단한 이미지 편집이 가능합니다.",
-                    imgSrc: "img-icon-photoshop"
+                    "name": "Photoshop",
+                    "description": "간단한 이미지 편집 작업에 사용했습니다.",
+                    "imgSrc": "img-icon-photoshop"
                 },
                 {
-                    name:"After Effect",
-                    description:"간단한 모션 작업(리깅, 퍼펫) 및 영상 편집이 가능합니다.",
-                    imgSrc: "img-icon-photoshop"
-                },
+                    "name": "After Effect",
+                    "description": "리깅과 퍼펫 작업을 포함한 간단한 모션 및 영상 편집을 했습니다.",
+                    "imgSrc": "img-icon-photoshop"
+                }
             ]
         },
         {
-            title: "협력도구",
-            desc: [
+            "title": "협력도구",
+            "desc": [
                 {
-                    name:"Jira",
-                    description:"간단한 이슈 등록 및 의견 작성등을 수행해봤습니다.",
+                    "name": "Jira",
+                    "description": "이슈 등록 및 의견 작성 등의 기본적인 작업을 수행했습니다."
                 },
                 {
-                    name:"Slack",
-                    description:"같이 작업하는 사람들과 손쉽게 코드를 공유 및 업무를 분배하기 용이한 대화수단이라 사용하게 되었습니다.",
+                    "name": "Slack",
+                    "description": "팀원들과 코드 공유 및 업무 분배를 용이하게 하기 위해 사용했습니다."
                 },
                 {
-                    name:"Teams",
-                    description:"React의 구조를 이해하고 있으며, 여러가지 Hook을 사용할 수 있습니다.",
+                    "name": "Teams",
+                    "description": "팀 내 소통과 협업을 위해 사용했습니다."
                 },
                 {
-                    name:"Red Mine",
-                    description:"이슈 등록 및 댓글 작성과 같은 기본적인 사용이 가능합니다.",
-                },
+                    "name": "Red Mine",
+                    "description": "이슈 등록 및 댓글 작성과 같은 기본적인 작업을 수행했습니다."
+                }
             ]
-        },
+        }
     ],
 
     // 섹션 4
